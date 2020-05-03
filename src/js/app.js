f71189c52383680a4d2e022b7982c2ec7201ac53
Nikita Dedkov <dedkovnd@gmail.com>
2020-05-03 23:51:41 +0300

    add homework's files

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
 // TODO: write your code here
 import sum from './basic';
 
+import Game, GameSavingData, {readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js';
+
+const game = new Game();
+game.start();
+
 console.log('worked');
 
 console.log(sum([1, 2]));
